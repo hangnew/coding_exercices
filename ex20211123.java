0cdb54cc05372531f2b6bcf43dc28f9a3126c149
행뉴 hangnew <86291359+hangnew@users.noreply.github.com>
2021-11-23 15:41:10 +0900

    (구름) 시간복잡도

---
+++ ex20211123.java
@@ -0,0 +1,23 @@
+// N이 커짐에 따라 N!의 값이 얼마나 빨리 커지는지 잘 모르는 사람들에게 알려주기 위해
+// N이 주어지면 N!의 맨 뒤에 붙는 연속한 0의 개수를 출력하는 프로그램을 작성 하려고 한다.
+// 1. 첫째 줄에 정수 N이 주어진다.
+// 2. N!의 맨 뒤에 붙는 연속한 0의 개수를 출력한다.
+
+import java.util.*;
+class Main {
+	public static int zeros(int number) {
+		int count = 0;
+		while (number >= 5) {
+			count += number / 5;
+			number /= 5;
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		System.out.println(zeros(n));
+	}
+}
